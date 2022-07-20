722b7bab75cdfb34a55d563e593e2272705110e5
litong <i@litong.dev>
2022-07-21 00:35:51 +0800

    feat: 使用ViewBinding自动生成Button1实例

--- app/src/main/java/com/example/activitytest/FirstActivity.kt
+++ app/src/main/java/com/example/activitytest/FirstActivity.kt
@@ -1,16 +1,20 @@
 package com.example.activitytest
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.activitytest.databinding.FirstLayoutBinding
 
 class FirstActivity : AppCompatActivity() {
+
+    private lateinit var binding: FirstLayoutBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.first_layout)
-        val button1: Button = findViewById(R.id.Button1)
-        button1.setOnClickListener {
+        binding = FirstLayoutBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        binding.Button1.setOnClickListener {
             Toast.makeText(this, "You clicked Button 1", Toast.LENGTH_SHORT).show()
         }
     }
