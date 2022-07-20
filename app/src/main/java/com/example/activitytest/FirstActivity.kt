44d20aae3cb2b28ceb2e561cf96fde926366bdae
litong <i@litong.dev>
2022-07-20 23:40:18 +0800

    feat: 创建和加载布局

--- app/src/main/java/com/example/activitytest/FirstActivity.kt
+++ app/src/main/java/com/example/activitytest/FirstActivity.kt
@@ -6,5 +6,6 @@ import android.os.Bundle
 class FirstActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.first_layout)
     }
 }
