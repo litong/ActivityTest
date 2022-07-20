8af898a86af20d4993761b38680883849d84364a
litong <i@litong.dev>
2022-07-21 00:08:38 +0800

    feat: 在Activity中使用Toast

--- app/src/main/java/com/example/activitytest/FirstActivity.kt
+++ app/src/main/java/com/example/activitytest/FirstActivity.kt
@@ -2,10 +2,16 @@ package com.example.activitytest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class FirstActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.first_layout)
+        val button1: Button = findViewById(R.id.Button1)
+        button1.setOnClickListener {
+            Toast.makeText(this, "You clicked Button 1", Toast.LENGTH_SHORT).show()
+        }
     }
 }
