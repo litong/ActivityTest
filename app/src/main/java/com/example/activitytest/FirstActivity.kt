23feb8dc68368df42e56338df25a58333b75c8a5
litong <i@litong.dev>
2022-07-21 01:02:55 +0800

    feat: 在Activity中使用Menu

--- app/src/main/java/com/example/activitytest/FirstActivity.kt
+++ app/src/main/java/com/example/activitytest/FirstActivity.kt
@@ -1,6 +1,8 @@
 package com.example.activitytest
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.activitytest.databinding.FirstLayoutBinding
@@ -17,5 +19,18 @@ class FirstActivity : AppCompatActivity() {
         binding.Button1.setOnClickListener {
             Toast.makeText(this, "You clicked Button 1", Toast.LENGTH_SHORT).show()
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.add_item -> Toast.makeText(this, "You clicked Add", Toast.LENGTH_SHORT).show()
+            R.id.remove_item -> Toast.makeText(this, "You clicked Remove", Toast.LENGTH_SHORT).show()
+        }
+        return true
     }
 }
